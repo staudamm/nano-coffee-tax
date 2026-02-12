5b32f3a1512fae432c6b8813f4095c245ebd5680
Benjamin Staude <stoide@googlemail.com>
2026-02-12 12:11:37 +0100

    Remove pandas dependency

---
+++ excel.py
@@ -0,0 +1,34 @@
+HEADER_ROW = 14
+
+TOTAL_EU = 'C9'
+TOTAL_EXPORT = 'C10'
+
+TIME_FROM = 'D6'
+TIME_TO = 'D7'
+
+source_idx_from_key = {
+    "Amount": 1,
+    "Country": 3,
+    "Name": 5,
+    "Address": 6,
+    "Zip": 7,
+    "City": 8,
+}
+
+
+row = {
+    "ID": 1,
+    "Sender": "NANO Kaffee GmbH, Charlottenstr. 1, 10969, Berlin",
+    "Place and Data of Export": "Berlin, XXXX",
+    "Amount": 0,
+    "Product": "100% Röstkaffee",
+    "Region": "",
+    "Country": "",
+    "Name": "",
+    "Address": "",
+    "Zip": "",
+    "City": "",
+    "VAT Number": "",
+    "Tracking URL": "",
+    "Reference": ""
+}
